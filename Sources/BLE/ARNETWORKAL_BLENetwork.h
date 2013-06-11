5bee822526780bf008a355c2876f7e5a38c4e54f
D'HAEYER Frédéric <frederic.dhaeyer@parrot.com>
2013-06-11 16:16:47 +0200

    Add BLE Network interface (in progress). DO NOT USE IT.

--- Sources/BLE/ARNETWORKAL_BLENetwork.h
+++ Sources/BLE/ARNETWORKAL_BLENetwork.h
@@ -7,8 +7,56 @@
 
 #ifndef _ARNETWORKAL_BLENETWORK_PRIVATE_H_
 #define _ARNETWORKAL_BLENETWORK_PRIVATE_H_
+
+#include <libARNetworkAL/ARNETWORKAL_Manager.h>
 #import "ARNETWORKAL_BLEManager.h"
 
+/**
+ *  @brief Create a new BLENetwork object.
+ *  @warning This function allocate memory
+ *  @post ARNETWORKAL_BLENetwork_Delete() must be called to delete the BLE network and free the memory allocated.
+ *  @param[in] manager address of the pointer on the Manager
+ *  @return eARNETWORKAL_ERROR
+ *  @see ARNETWORKAL_BLENetwork_Delete()
+ */
+eARNETWORKAL_ERROR ARNETWORKAL_BLENetwork_New (ARNETWORKAL_Manager_t *manager);
 
+/**
+ *  @brief Delete the BLENetwork
+ *  @warning This function free memory
+ *  @param[in] manager address of the pointer on the Manager
+ *  @see ARNETWORKAL_BLENetwork_New()
+ */
+eARNETWORKAL_ERROR ARNETWORKAL_BLENetwork_Delete (ARNETWORKAL_Manager_t *manager);
+
+/**
+ *  @brief Callback defines to push next frame to send to Network.
+ *  @param[in] manager address of the pointer on the Manager
+ *	@param[in] frame frame to push
+ *  @return error equal to ARNETWORKAL_MANAGER_CALLBACK_DEFAULT if the the next frame pushed on success otherwise error in eARNETWORKAL_MANAGER_CALLBACK_RETURN.
+ **/
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_pushNextFrameCallback(ARNETWORKAL_Manager_t *manager, ARNETWORKAL_Frame_t *frame);
+
+/**
+ *  @brief Callback defines to pop next frame received from Network.
+ *  @param[in] manager address of the pointer on the Manager
+ *	@param[in] frame frame to pop
+ *  @return error equal to ARNETWORKAL_MANAGER_CALLBACK_DEFAULT if the the next frame pushed on success otherwise error in eARNETWORKAL_MANAGER_CALLBACK_RETURN.
+ **/
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_popNextFrameCallback(ARNETWORKAL_Manager_t *manager, ARNETWORKAL_Frame_t *frame);
+
+/**
+ *  @brief Callback defines to send all frames to Network.
+ *  @param[in] manager address of the pointer on the Manager
+ *  @return error equal to ARNETWORKAL_MANAGER_CALLBACK_DEFAULT if the the next frame pushed on success otherwise error in eARNETWORKAL_MANAGER_CALLBACK_RETURN.
+ **/
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_sendingCallback(ARNETWORKAL_Manager_t *manager);
+
+/**
+ *  @brief Callback defines to receive frames from Network.
+ *  @param[in] manager address of the pointer on the Manager
+ *  @return error equal to ARNETWORKAL_MANAGER_CALLBACK_DEFAULT if the the next frame pushed on success otherwise error in eARNETWORKAL_MANAGER_CALLBACK_RETURN.
+ **/
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_receivingCallback(ARNETWORKAL_Manager_t *manager);
 
 #endif /** _ARNETWORKAL_BLENETWORK_PRIVATE_H_ */
--- Sources/BLE/ARNETWORKAL_BLENetwork.m
+++ Sources/BLE/ARNETWORKAL_BLENetwork.m
@@ -10,4 +10,297 @@
  *             inport file :
  *
  *****************************************/
+#include <libARSAL/ARSAL_Endianness.h>
 #import "ARNETWORKAL_BLENetwork.h"
+
+#define ARNETWORKAL_BLENETWORK_TAG 					"ARNETWORKAL_BLENetwork"
+#define ARNETWORKAL_BLENETWORK_SENDING_BUFFER_SIZE		20
+#define ARNETWORKAL_BLENETWORK_RECEIVING_BUFFER_SIZE	20
+
+/*****************************************
+ *
+ *             private header:
+ *
+ *****************************************/
+typedef struct _ARNETWORKAL_BLENetworkObject_
+{
+	CBPeripheral *peripheral;
+	uint8_t *buffer;
+	uint8_t *currentFrame;
+	uint32_t size;
+} ARNETWORKAL_BLENetworkObject;
+
+/*****************************************
+ *
+ *             implementation :
+ *
+ *****************************************/
+eARNETWORKAL_ERROR ARNETWORKAL_BLENetwork_New (ARNETWORKAL_Manager_t *manager)
+{
+    eARNETWORKAL_ERROR error = ARNETWORKAL_OK;
+    
+    /** Check parameters */
+    if(manager == NULL)
+    {
+    	error = ARNETWORKAL_ERROR_BAD_PARAMETER;
+    }
+    
+    /** Allocate sender object */
+    if(error == ARNETWORKAL_OK)
+    {
+    	manager->senderObject = malloc(sizeof(ARNETWORKAL_BLENetworkObject));
+    	if(manager->senderObject != NULL)
+    	{
+    		((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->peripheral = nil;
+    	}
+    	else
+    	{
+    		error = ARNETWORKAL_ERROR_ALLOC;
+    	}
+    }
+    
+    /** Allocate sender buffer */
+    if(error == ARNETWORKAL_OK)
+    {
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer = (uint8_t *)malloc(sizeof(uint8_t) * ARNETWORKAL_BLENETWORK_SENDING_BUFFER_SIZE);
+    	if(((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer != NULL)
+    	{
+    		((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size = 0;
+    	    ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame = ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer;
+    	}
+    	else
+    	{
+    		error = ARNETWORKAL_ERROR_ALLOC;
+    	}
+    }
+    
+    /** Allocate receiver object */
+    if(error == ARNETWORKAL_OK)
+    {
+    	manager->receiverObject = malloc(sizeof(ARNETWORKAL_BLENetworkObject));
+    	if(manager->receiverObject != NULL)
+    	{
+    		((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->peripheral = nil;
+     	}
+    	else
+    	{
+    		error = ARNETWORKAL_ERROR_ALLOC;
+    	}
+    }
+    
+    /** Allocate receiver buffer */
+    if(error == ARNETWORKAL_OK)
+    {
+    	((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer = (uint8_t *)malloc(sizeof(uint8_t) * ARNETWORKAL_BLENETWORK_RECEIVING_BUFFER_SIZE);
+    	if(((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer != NULL)
+    	{
+    		((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size = 0;
+    	}
+    	else
+    	{
+    		error = ARNETWORKAL_ERROR_ALLOC;
+    	}
+    }
+    
+    return error;
+}
+
+eARNETWORKAL_ERROR ARNETWORKAL_BLENetwork_Delete (ARNETWORKAL_Manager_t *manager)
+{
+    eARNETWORKAL_ERROR error = ARNETWORKAL_OK;
+    
+    if(manager == NULL)
+    {
+    	error = ARNETWORKAL_ERROR_BAD_PARAMETER;
+    }
+    
+    if(error == ARNETWORKAL_OK)
+    {
+		if (manager->senderObject)
+		{
+			//ARSAL_Socket_Close(((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->socket);
+            
+			if(((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer)
+			{
+			    free (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer);
+			    ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer = NULL;
+			}
+            
+		    free (manager->senderObject);
+			manager->senderObject = NULL;
+		}
+        
+		if(manager->senderObject)
+		{
+			//ARSAL_Socket_Close(((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->socket);
+            
+			if(((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer)
+			{
+			    free (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer);
+			    ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer = NULL;
+			}
+            
+			free (manager->receiverObject);
+			manager->receiverObject = NULL;
+		}
+    }
+    
+    return error;
+}
+
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_pushNextFrameCallback(ARNETWORKAL_Manager_t *manager, ARNETWORKAL_Frame_t *frame)
+{
+    eARNETWORKAL_MANAGER_CALLBACK_RETURN result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT;
+    
+    if((((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size + frame->size) > ARNETWORKAL_BLENETWORK_SENDING_BUFFER_SIZE)
+    {
+    	result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_BUFFER_FULL;
+    }
+    
+    if(result == ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT)
+    {
+        uint32_t droneEndianUInt32 = 0;
+        
+    	/** Add type */
+    	memcpy (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame, &frame->type, sizeof (uint8_t));
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame += sizeof (uint8_t);
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size += sizeof (uint8_t);
+        
+        /** Add frame type */
+    	memcpy (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame, &frame->id, sizeof (uint8_t));
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame += sizeof (uint8_t);
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size += sizeof (uint8_t);
+        
+    	/** Add frame sequence number */
+    	droneEndianUInt32 = htodl (frame->seq);
+    	memcpy (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame, &droneEndianUInt32, sizeof (uint32_t));
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame += sizeof (uint32_t);
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size += sizeof (uint32_t);
+        
+    	/** Add frame size */
+    	droneEndianUInt32 =  htodl (frame->size);
+       	memcpy (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame, &droneEndianUInt32, sizeof (uint32_t));
+       	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame += sizeof (uint32_t);
+        ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size += sizeof (uint32_t);
+        
+        /** Add frame data */
+        uint32_t dataSize = frame->size - offsetof (ARNETWORKAL_Frame_t, dataPtr);
+        
+      	memcpy (((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame, frame->dataPtr, dataSize);
+      	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame += dataSize;
+        ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size += dataSize;
+    }
+    
+    return result;
+}
+
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_popNextFrameCallback(ARNETWORKAL_Manager_t *manager, ARNETWORKAL_Frame_t *frame)
+{
+    eARNETWORKAL_MANAGER_CALLBACK_RETURN result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT;
+    
+    /** -- get a Frame of the receiving buffer -- */
+    /** if the receiving buffer not contain enough data for the frame head*/
+    if (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame > ((((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer + ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size) - offsetof (ARNETWORKAL_Frame_t, dataPtr)))
+    {
+        if (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame == (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer + ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size))
+        {
+            result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_BUFFER_EMPTY;
+        }
+        else
+        {
+            result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_BAD_FRAME;
+        }
+    }
+    
+    if (result == ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT)
+    {
+        /** Get the frame from the buffer */
+        /** get type */
+        memcpy (&(frame->type), ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame, sizeof (uint8_t));
+        ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame += sizeof (uint8_t) ;
+        
+        /** get id */
+        memcpy (&(frame->id), ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame, sizeof (uint8_t));
+        ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame += sizeof (uint8_t);
+        
+        /** get seq */
+        memcpy (&(frame->seq), ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame, sizeof (uint32_t));
+        ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame += sizeof (uint32_t);
+        /** convert the endianness */
+        frame->seq = dtohl (frame->seq);
+        
+        /** get size */
+        memcpy (&(frame->size), ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame, sizeof (uint32_t));
+        ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame += sizeof(uint32_t);
+        /** convert the endianness */
+        frame->size = dtohl (frame->size);
+        
+        /** get data address */
+        frame->dataPtr = ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame;
+        
+        /** if the receiving buffer not contain enough data for the full frame */
+        if (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame > ((((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer + ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size) - (frame->size - offsetof (ARNETWORKAL_Frame_t, dataPtr))))
+        {
+         	result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_BAD_FRAME;
+        }
+    }
+    
+    if (result == ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT)
+    {
+        /** offset the readingPointer on the next frame */
+    	((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame = ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame + frame->size - offsetof (ARNETWORKAL_Frame_t, dataPtr);
+    }
+    else
+    {
+        /** reset the reading pointer to the start of the buffer */
+    	((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame = ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer;
+    	((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size = 0;
+        
+        /** reset frame */
+        frame->type = ARNETWORKAL_FRAME_TYPE_UNINITIALIZED;
+        frame->id = 0;
+        frame->seq = 0;
+        frame->size = 0;
+        frame->dataPtr = NULL;
+    }
+    
+    return result;
+}
+
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_sendingCallback(ARNETWORKAL_Manager_t *manager)
+{
+    eARNETWORKAL_MANAGER_CALLBACK_RETURN result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT;
+    
+    if(((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size != 0)
+    {
+    	//ARSAL_Socket_Send(((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->socket, ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer, ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size, 0);
+    	((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->size = 0;
+	    ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->currentFrame = ((ARNETWORKAL_BLENetworkObject *)manager->senderObject)->buffer;
+    }
+    
+    return result;
+}
+
+eARNETWORKAL_MANAGER_CALLBACK_RETURN ARNETWORKAL_BLENetwork_receivingCallback(ARNETWORKAL_Manager_t *manager)
+{
+    eARNETWORKAL_MANAGER_CALLBACK_RETURN result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_DEFAULT;
+    
+    /** -- receiving data present on the socket -- */
+    /** local declarations */
+    int size = 0;
+    //int size = ARSAL_Socket_Recv (((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->socket, ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer, ARNETWORKAL_BLENETWORK_RECEIVING_BUFFER_SIZE, 0);
+    
+	if (size > 0)
+	{
+        ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size = size;
+	}
+	else
+	{
+		result = ARNETWORKAL_MANAGER_CALLBACK_RETURN_NO_DATA_AVAILABLE;
+		((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->size = 0;
+	}
+    
+	((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->currentFrame = ((ARNETWORKAL_BLENetworkObject *)manager->receiverObject)->buffer;
+    
+    return result;
+}
