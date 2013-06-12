093940fde6aca57d73884d6303668d1a2eb3a0d6
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-06-12 16:20:46 +0200

    Fixed some issues with enum names + fixed java part

--- JNI/java/ARNetworkALManager.java
+++ JNI/java/ARNetworkALManager.java
@@ -5,8 +5,8 @@ import com.parrot.arsdk.arsal.ARNativeData;
 
 /**
  * Network manager allow to send and receive on network.
-**/
-public class ARNetworkALManager 
+ **/
+public class ARNetworkALManager
 {
     private static final String TAG = "ARNetworkALManager";
 
@@ -16,27 +16,25 @@ public class ARNetworkALManager
 
     private long m_managerPtr;
     private boolean m_initOk;
-    
+
     /**
      *  Constructor
-    **/
-    public ARNetworkALManager() 
-    {
+     **/
+    public ARNetworkALManager() {
         m_initOk = false;
         m_managerPtr = nativeNew();
-                
+
         if( m_managerPtr != 0 )
         {
-            m_initOk = true; 
+            m_initOk = true;
         }
     }
 
     /**
      *  Dispose
      *  @post after this function the object must be not used more
-    **/
-    public void dispose() 
-    {
+     **/
+    public void dispose() {
         if(m_initOk == true)
         {
             nativeDelete(m_managerPtr);
@@ -44,50 +42,47 @@ public class ARNetworkALManager
             m_initOk = false;
         }
     }
-    
+
     /**
      *  Destructor
-    **/
-    public void finalize () throws Throwable
-    {
+     **/
+    public void finalize () throws Throwable {
         try {
             dispose ();
         } finally {
             super.finalize ();
         }
     }
-    
-     /**
-     *  Get the pointer C on the network manager 
-     *  @return  Pointer C on the network manager 
-    **/
-    public long getManager () 
-    {
-        return m_managerPtr;
-    }
-    
+
+    /**
+     *  Get the pointer C on the network manager
+     *  @return  Pointer C on the network manager
+     **/
+    public long getManager ()
+        {
+            return m_managerPtr;
+        }
+
     /**
      *  Get is the Manager is correctly initialized and if it is usable
      *  @return true is the Manager is usable
-    **/
-    public boolean isCorrectlyInitialized () 
-    {
+     **/
+    public boolean isCorrectlyInitialized () {
         return m_initOk;
     }
-    
+
     /**
      *  Initialize Wifi network to send and receive data
-    **/
-    public eARNETWORKAL_ERROR initWiFiNetwork(String addr, int sendingPort, int receivingPort, int recvTimeoutSec) 
-    {
-    	eARNETWORKAL_ERROR error = eARNETWORKAL_ERROR.ARNETWORKAL_ERROR;
-    	
-    	if(addr != null)
+     **/
+    public ARNETWORKAL_ERROR_ENUM initWiFiNetwork(String addr, int sendingPort, int receivingPort, int recvTimeoutSec) {
+        ARNETWORKAL_ERROR_ENUM error = ARNETWORKAL_ERROR_ENUM.ARNETWORKAL_ERROR;
+
+        if(addr != null)
         {
             int intError = nativeInitWiFiNetwork(m_managerPtr, addr, sendingPort, receivingPort, recvTimeoutSec);
-            error =  eARNETWORKAL_ERROR.getErrorName(intError); 
+            error =  ARNETWORKAL_ERROR_ENUM.getFromValue(intError);
         }
-    	
-    	return error;
+
+        return error;
     }
 }
