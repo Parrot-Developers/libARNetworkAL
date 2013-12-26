db95f244293b9262b08f55ecbf7e25b7f051463c
Nicolas Brûlez <nicolas.brulez@parrot.com>
2013-12-26 14:42:15 +0100

    Added a generic ARNetworkAL.h header which includes all ARNetworkAL headers

---
+++ Includes/libARNetworkAL/ARNetworkAL.h
@@ -0,0 +1,15 @@
+/**
+ * @file ARNetworkAL.h
+ * @brief Global header for libARNetworkAL
+ * @date 12/04/2013
+ * @author nicolas.brulez@parrot.com
+ */
+
+#ifndef _ARNETWORKAL_H_
+#define _ARNETWORKAL_H_
+
+#include <libARNetworkAL/ARNETWORKAL_Error.h>
+#include <libARNetworkAL/ARNETWORKAL_Manager.h>
+#include <libARNetworkAL/ARNETWORKAL_Frame.h>
+
+#endif /* _ARNETWORKAL_H_ */
