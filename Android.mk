5ed8ba055cf3eb2c9ddc0a637d5e1482571cab8e
Maxime MAITRE <maxime.maitre@parrot.com>
2016-06-02 14:12:59 +0200

    [BUILD] Use Android.mk generated by alchemy.
    Change-Id: I3fa896b0c92298c5086a9b421cd48fa78234bd18
    Reviewed-on: https://gerrit.parrot.biz/40044
    Reviewed-by: Djavan Bertrand <djavan.bertrand@parrot.com>

--- Android.mk
+++ Android.mk
@@ -7,5 +7,8 @@ LOCAL_CFLAGS := -g
 LOCAL_MODULE := libarnetworkal_android
 LOCAL_SRC_FILES := JNI/c/ARNETWORKAL_JNIManager.c JNI/c/ARNETWORKAL_JNIBLENetwork.c
 LOCAL_LDLIBS := -llog -lz
-LOCAL_SHARED_LIBRARIES := libARNetworkAL-prebuilt libARSAL-prebuilt
+LOCAL_SHARED_LIBRARIES := \
+	libARNetworkAL \
+	libARSAL
+
 include $(BUILD_SHARED_LIBRARY)
